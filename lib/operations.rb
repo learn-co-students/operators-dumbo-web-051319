596284d9bdf47c87d00a0bdf500c342cbeda1871
Lorena Ramirez-Lopez <daleloreny@gmail.com>
2019-04-22 14:47:08 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,18 +1,17 @@
 def unsafe?(speed)
-if speed > 40 
-  "true"
-
-elsif speed < 60
-  "true"
-  
-else
-  "false"
+  if speed < 40 
+    true
+  elsif speed > 60
+    true
+  else
+    false
   end
 end
 
 
 def not_safe?(speed)
-	
+	speed < 40 || speed > 60 ? true : false
+	# using comparison operators || (double pipeline) to signify OR and then after writing that condition added the ternary operator ? :
 end
 	
 
